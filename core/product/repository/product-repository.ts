a17d65053497245945a36bb9ab4bd6505c5d0b9a
Ayob Malkochi <a.malkochi654@gmail.com>
2026-08-04 20:56:28 +0330

    Build Product Repository Layer

--- core/product/repository/product-repository.ts
+++ core/product/repository/product-repository.ts
@@ -4,40 +4,97 @@ import { Product } from "../models/product-model";
 export class ProductRepository {
 
 
-    private products: Product[] = [];
+private products: Product[] = [];
 
 
-    add(product: Product){
 
-        this.products.push(product);
+add(product: Product){
 
-    }
+this.products.push(product);
 
+return product;
 
-    getAll(){
-
-        return this.products;
-
-    }
+}
 
 
-    findById(id:string){
 
-        return this.products.find(
-            product => product.id === id
-        );
+getAll(): Product[]{
 
-    }
+return this.products;
+
+}
 
 
-    search(keyword:string){
 
-        return this.products.filter(product =>
-            product.name.includes(keyword) ||
-            product.category.includes(keyword) ||
-            (product.tags?.includes(keyword) ?? false)
-        );
+findById(id:string){
 
-    }
+return this.products.find(
+product => product.id === id
+);
+
+}
+
+
+
+search(keyword:string){
+
+
+const key = keyword.toLowerCase();
+
+
+
+return this.products.filter(product =>
+
+
+product.name.toLowerCase().includes(key) ||
+
+product.category.toLowerCase().includes(key) ||
+
+(product.tags?.some(
+tag => tag.toLowerCase().includes(key)
+) ?? false)
+
+
+);
+
+
+
+}
+
+
+
+update(id:string,data:Partial<Product>){
+
+const product = this.findById(id);
+
+
+if(!product){
+
+return null;
+
+}
+
+
+Object.assign(product,data);
+
+
+return product;
+
+}
+
+
+
+remove(id:string){
+
+this.products =
+this.products.filter(
+product => product.id !== id
+);
+
+
+return true;
+
+}
+
 
 }
