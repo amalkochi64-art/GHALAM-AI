ec47e40810b35613cd3564201f764da80e1e802e
Ayob Malkochi <a.malkochi654@gmail.com>
2026-08-04 21:07:56 +0330

    Build Product Service Layer

--- core/product/service/product-service.ts
+++ core/product/service/product-service.ts
@@ -1,89 +1,46 @@
 import { Product } from "../models/product-model";
 import { ProductRepository } from "../repository/product-repository";
-import { ProductBrain } from "../brain/product-brain";
-
 
 export class ProductService {
 
-
-private repository =
-new ProductRepository();
+    private repository = new ProductRepository();
 
 
-private brain =
-new ProductBrain();
+    create(product: Product){
+        return this.repository.add(product);
+    }
 
 
-
-create(product:Product){
-
-return this.repository.add(product);
-
-}
+    getProducts(){
+        return this.repository.getAll();
+    }
 
 
-
-getProducts(){
-
-return this.repository.getAll();
-
-}
+    getProduct(id:string){
+        return this.repository.findById(id);
+    }
 
 
-
-getProduct(id:string){
-
-return this.repository.findById(id);
-
-}
+    searchProducts(keyword:string){
+        return this.repository.search(keyword);
+    }
 
 
+    updateProduct(id:string,data:Partial<Product>){
 
-search(keyword:string){
+        return this.repository.update(
+            id,
+            data
+        );
 
-return this.repository.search(keyword);
-
-}
-
-
-
-analyze(product:Product){
-
-return this.brain.analyze(product);
-
-}
+    }
 
 
+    deleteProduct(id:string){
 
-recommend(){
+        return this.repository.remove(id);
 
-return this.brain.recommend(
-this.repository.getAll()
-);
-
-}
-
-
-
-update(
-id:string,
-data:Partial<Product>
-){
-
-return this.repository.update(
-id,
-data
-);
-
-}
-
-
-
-delete(id:string){
-
-return this.repository.remove(id);
-
-}
+    }
 
 
 }
