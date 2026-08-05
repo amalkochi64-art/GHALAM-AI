8a589be4a1e4e959cbf20db1aab9f4211207b896
Ayob Malkochi <a.malkochi654@gmail.com>
2026-08-05 11:56:56 +0330

    Fix Application Container V2 Import Architecture

---
+++ core/container/v2/application-container.ts
@@ -0,0 +1,60 @@
+﻿import {SalesEngine} from "../../sales/sales-engine";
+import {OfferWorkflow} from "../../sales/offer/offer-workflow";
+import {ApprovalService} from "../../admin/approval/service/approval-service";
+import {EventBus} from "../../events/event-bus";
+import {CustomerProfileService} from "../../customer/profile/customer-profile-service";
+import {AnalyticsEngine} from "../../analytics/analytics-engine";
+import {SecurityService} from "../../security/security-service";
+
+
+export class ApplicationContainer{
+
+
+sales:SalesEngine;
+
+approval:ApprovalService;
+
+offerWorkflow:OfferWorkflow;
+
+events:EventBus;
+
+customer:CustomerProfileService;
+
+analytics:AnalyticsEngine;
+
+security:SecurityService;
+
+
+
+constructor(){
+
+
+this.events=new EventBus();
+
+
+this.approval=new ApprovalService();
+
+
+this.offerWorkflow=new OfferWorkflow(
+this.approval,
+this.events
+);
+
+
+
+this.sales=new SalesEngine();
+
+
+this.customer=new CustomerProfileService();
+
+
+this.analytics=new AnalyticsEngine();
+
+
+this.security=new SecurityService();
+
+
+}
+
+
+}
