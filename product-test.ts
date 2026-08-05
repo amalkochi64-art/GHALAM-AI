7db3cc1d6e1089ed76d719c148cd016a41103e43
Ayob Malkochi <a.malkochi654@gmail.com>
2026-08-05 07:29:50 +0330

    Update Product Test for Product Service

--- product-test.ts
+++ product-test.ts
@@ -1,49 +1,23 @@
 import { ProductService } from "./core/product/service/product-service";
 
-
-const service =
-new ProductService();
-
-
+const service = new ProductService();
 
 service.create({
-
-id:"chair-001",
-
-name:"Executive Office Chair",
-
-category:"Office Chair",
-
-price:15000000,
-
-stock:10,
-
-sales:120,
-
-tags:[
-"„œÌ—Ì Ì",
-"·Êò”",
-"ç—„Ì"
-]
-
+    id:"chair-001",
+    name:"Executive Office Chair",
+    category:"Office Chair",
+    price:15000000,
+    stock:10,
+    sales:120,
+    tags:[
+        "„œÌ—Ì Ì",
+        "·Êò”",
+        "ç—„Ì"
+    ]
 });
 
-
-
-console.log(
-service.getProducts()
-);
-
-
+console.log(service.getProducts());
 
 console.log(
-service.recommend()
-);
-
-
-
-console.log(
-service.analyze(
-service.getProducts()[0]
-)
+    service.searchProducts("chair")
 );
